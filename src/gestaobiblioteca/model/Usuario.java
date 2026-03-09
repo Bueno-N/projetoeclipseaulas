fc03705382f95d0e366117883211cce1c794f16f
Nicolas Bueno <nicolas-bb@hotmail.com>
2026-03-09 22:20:27 +0000

    aula1

--- src/gestaobiblioteca/model/Usuario.java
+++ src/gestaobiblioteca/model/Usuario.java
@@ -1,1 +1,17 @@
 package gestaobiblioteca.model;
+
+public class Usuario{
+    private int id;
+
+    public void setID(int id) {
+        if(id <= 0) {
+            throw new IllegalArgumentException("id deve ser um numero positivo");
+
+        }
+        this.id = id;
+    }
+    
+    public int getID(){
+        return this.id;
+    }
+}
--- src/gestaobiblioteca/view/TelaUsurio.java
+++ src/gestaobiblioteca/view/TelaUsurio.java
@@ -1,1 +1,17 @@
-package gestaobiblioteca.view;
+package gestaobiblioteca.model.view;
+
+import gestaobiblioteca.model.Usuario;
+
+public class TelaUsuario {
+    public static void main(String[] args){
+        System.out.println("Bem vindo a tela de usuario");
+
+        Usuario usuario1 = new Usuario() {
+
+        };
+
+        usuario1.setId(-2);
+        System.out.println("ID do usuário: " + usuario1.getId());
+
+    }
+}
