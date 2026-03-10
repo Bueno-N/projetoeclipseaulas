d9ab28652f6cdc849e63eec9b1a2d7c4325bba28
Nicolas Bueno <nicolas-bb@hotmail.com>
2026-03-10 00:16:57 +0000

    aula 2 correcao

--- src/gestaobiblioteca/model/Administrador.java
+++ src/gestaobiblioteca/model/Administrador.java
@@ -3,4 +3,5 @@ package gestaobiblioteca.model;
 public class Administrador extends Usuario {
     public Administrador(long id, String nome, String email, String login, String senha, Perfil perfil){
         super(id, nome, email, login, senha, Perfil.ADMIN);
+    }
 }
--- src/gestaobiblioteca/model/Professor.java
+++ src/gestaobiblioteca/model/Professor.java
@@ -3,4 +3,5 @@ package gestaobiblioteca.model;
 public class Professor extends Usuario{
     public Professor(long id, String nome, String email, String login, String senha, Perfil perfil){
         super(id, nome, email, login, senha, Perfil.PROFESSOR);
+    }
 }
